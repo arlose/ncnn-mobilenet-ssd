893c7c4ac09b324294f40f77b2c7731d8fba65ea
arlose <arlose.fj@gmail.com>
2017-09-30 14:13:11 +0800

    input

--- ncnn-mobilenet-ssd/ssdmobilenet/ssdmobilenet.cpp
+++ ncnn-mobilenet-ssd/ssdmobilenet/ssdmobilenet.cpp
@@ -29,8 +29,8 @@ static int detect_mobilenet(const cv::Mat& bgr, std::vector<float>& cls_scores)
 
     ncnn::Mat in = ncnn::Mat::from_pixels_resize(bgr.data, ncnn::Mat::PIXEL_BGR, bgr.cols, bgr.rows, 300, 300);
 
-    //const float mean_vals[3] = {104.f, 117.f, 123.f};
-    //in.substract_mean_normalize(mean_vals, 0);
+    const float mean_vals[3] = {127.5f, 127.5f, 127.5f};
+    in.substract_mean_div(mean_vals, 127.5f);
 
     // ncnn::Extractor ex = mobilenet.create_extractor();
     // ex.set_light_mode(true);
